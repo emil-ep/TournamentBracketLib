f4d794393ce2febc65687aa4a16d6238ee11ca4c
Emil <emilpradeep@gmail.com>
2021-06-18 22:28:16 +0530

    added necessary java files and imported the correct paths

---
+++ BracketsLib/src/main/java/com/ventura/bracketslib/utility/BracketsUtility.java
@@ -0,0 +1,17 @@
+package com.ventura.bracketslib.utility;
+
+import android.util.DisplayMetrics;
+
+import com.ventura.emilp.tournamentbrackets.application.BracketsApplication;
+
+/**
+ * Created by Emil on 21/10/17.
+ */
+
+public class BracketsUtility {
+    public static int dpToPx(int dp) {
+        DisplayMetrics displayMetrics = BracketsApplication.getInstance().getBaseContext().getResources().getDisplayMetrics();
+        return Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+    }
+
+}
