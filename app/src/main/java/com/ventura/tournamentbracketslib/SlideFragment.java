608841ab47d21dd5523fe5c3d039d2eb5af546d9
Emil <emilpradeep@gmail.com>
2021-08-25 22:23:06 +0530

    added fix while creating bracketsview using a fragment

---
+++ app/src/main/java/com/ventura/tournamentbracketslib/SlideFragment.java
@@ -0,0 +1,22 @@
+package com.ventura.tournamentbracketslib;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.ventura.tournamentbracketslib.R;
+
+public class SlideFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable  ViewGroup container,
+                             @Nullable  Bundle savedInstanceState) {
+        return  inflater.inflate(R.layout.layout_slide_fragment, container, false);
+    }
+}
